9bd4997b2fbcfa384065068bf788181bc30b7d7f
John Phillip <Phillip28@jaypee.github.com>
2018-09-19 00:32:58 +0800

    Snake game

---
+++ Snake.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+	<link href="styles/snake.css" rel="stylesheet" type="text/css">
+	<script src="js/snake.js"></script>
+	<title>Snake</title>
+</head>
+<body>
+
+<h1>Twisted Snake Game</h1>
+<canvas id="stage" height="500" width="620"></canvas>
+</body>
+</html>
